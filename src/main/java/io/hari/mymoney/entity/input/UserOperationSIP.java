91ac8eaab118881206771882c5bcf50cdfcf763e
Hariom Yadav <hoc.yadav@gmail.com>
2021-05-08 21:30:30 +0530

    update input

---
+++ src/main/java/io/hari/mymoney/entity/input/UserOperationSIP.java
@@ -0,0 +1,35 @@
+package io.hari.mymoney.entity.input;
+
+import io.hari.mymoney.constant.ActionType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigInteger;
+
+/**
+ * @Author hayadav
+ * @create 5/8/2021
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class UserOperationSIP extends UserOperation {
+
+    public UserOperationSIP(ActionType operation) {
+        super(operation);
+    }
+
+    private BigInteger equity;
+    private BigInteger dept;
+    private BigInteger gold;
+
+    @Builder
+    public UserOperationSIP(ActionType operation, BigInteger equity, BigInteger dept, BigInteger gold) {
+        super(operation);
+        this.equity = equity;
+        this.dept = dept;
+        this.gold = gold;
+    }
+}
