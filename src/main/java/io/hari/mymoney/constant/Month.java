fce238c8e8c3b868b65bfdef7b08f24a984e9a21
Hariom Yadav <hoc.yadav@gmail.com>
2021-05-08 21:31:06 +0530

    update constant

---
+++ src/main/java/io/hari/mymoney/constant/Month.java
@@ -0,0 +1,20 @@
+package io.hari.mymoney.constant;
+
+/**
+ * @Author hayadav
+ * @create 5/8/2021
+ */
+public enum Month {
+    january,
+    february,
+    march,
+    april,
+    may,
+    june,
+    july,
+    august,
+    september,
+    october,
+    november,
+    december;
+}
