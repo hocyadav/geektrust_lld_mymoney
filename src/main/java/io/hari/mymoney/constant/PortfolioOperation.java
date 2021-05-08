77fb3baffe49b3d2fbbb920c6678415761709ec2
Hariom Yadav <hoc.yadav@gmail.com>
2021-05-08 21:31:11 +0530

    update constant

---
+++ src/main/java/io/hari/mymoney/constant/PortfolioOperation.java
@@ -0,0 +1,13 @@
+package io.hari.mymoney.constant;
+
+/**
+ * @Author hayadav
+ * @create 5/8/2021
+ */
+public enum PortfolioOperation {
+    allocation,
+    existing,
+    after_market_change,
+    after_sip,
+    re_balance;
+}
---
+++ src/main/java/io/hari/mymoney/constant/UserType.java
@@ -0,0 +1,10 @@
+package io.hari.mymoney.constant;
+
+/**
+ * @Author hayadav
+ * @create 5/8/2021
+ */
+public enum UserType {
+    investor,
+    admin;
+}
