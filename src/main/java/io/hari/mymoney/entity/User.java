eb065a14374b69f2b7a7785c09c421f9efddd28a
Hariom Yadav <hoc.yadav@gmail.com>
2021-05-08 21:29:53 +0530

    update entity

---
+++ src/main/java/io/hari/mymoney/entity/User.java
@@ -0,0 +1,30 @@
+package io.hari.mymoney.entity;
+
+
+import io.hari.mymoney.constant.UserType;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author hayadav
+ * @create 5/8/2021
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class User {
+    @NotNull
+    String name;
+
+    @NotNull
+    UserType type;
+
+    @Builder.Default
+    List<Portfolio> portfolios = new LinkedList<>();
+}
